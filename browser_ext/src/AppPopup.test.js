af159649b55065640fcddd83e4d7364c2a0ee81c
mayne <dreama9ain@gmail.com>
2020-03-11 22:16:36 +0800

    rm: browser_ext

--- browser_ext/src/AppPopup.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppPopup from './AppPopup';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppPopup />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- browser_ext/src/options.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppOptions from './AppOptions';
-
-ReactDOM.render(<AppOptions />, document.getElementById('root'));
